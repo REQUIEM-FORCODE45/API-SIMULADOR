88da35d744ebaed23be4255bd4a6e4a48c895ecb
David Diaz <sinfisis0123@gmail.com>
2024-02-14 21:12:51 -0700

    New project tree

--- run.py
+++ run.py
@@ -5,21 +5,24 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/test', methods = ['GET', 'POST'])
+@app.route('/')
+def main():
+    return "<p> welcome to API simulator </p>"
 
+@app.route('/test', methods = ['GET', 'POST'])
 def test():
     if request.method == 'GET':
         return jsonify({"response": "Get requet called"})    
     elif request.method == "POST":
         reg_json = request.json
-        with open('input.json', 'w') as f:
+        with open('input/data/input.json', 'w') as f:
             json.dump(reg_json, f)
             
-        command = [ 'gridlabd', 'input.json', '-o', 'templates/output.json']
+        command = [ 'gridlabd', 'input.json', '-o', 'output.json']
         result = subprocess.run(command, capture_output=True, text=True)
         
         SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-        json_url = os.path.join(SITE_ROOT, "templates", "output.json")
+        json_url = os.path.join(SITE_ROOT,"raw" ,"output.json")
         data = json.load(open(json_url))        
         return jsonify(data)
 
